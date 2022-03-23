25116bb02b5798b3e1270e36fef6f428f1c5a3f4
liq19ch <s4031630087@gmail.com>
2022-03-24 04:21:01 +0800

    controller updated from return data to html

--- src/main/java/com/crm/demo/controller/CustomerController.java
+++ src/main/java/com/crm/demo/controller/CustomerController.java
@@ -3,20 +3,19 @@ package com.crm.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.crm.demo.entity.Customer;
 import com.crm.demo.entity.service.CustomerService;
 
-@RestController
-@RequestMapping("/api")
+@Controller
+@RequestMapping("/customers")
 public class CustomerController {
 
 	private CustomerService customerService;
@@ -26,41 +25,39 @@ public class CustomerController {
 		this.customerService = customerService;
 	}
 
-	@GetMapping("/customers")
-	public List<Customer> listAll() {
-		return customerService.getCustomers();
+	@GetMapping("/list")
+	public String listAll(Model model) {
+		List<Customer> customers = customerService.getCustomers();
+		model.addAttribute("customers", customers);
+		return "customers/list-customers";
 	}
 
-	@GetMapping("/customers/{customerId}")
-	public Customer getCustomer(@PathVariable int customerId) {
-		Customer customer = customerService.getCustomer(customerId);
-		if (customer == null) {
-			throw new RuntimeException("Customer is not found with id : " + customerId);
-		}
-		return customer;
-	}
-
-	@PostMapping("/customers")
-	public Customer addCustomer(@RequestBody Customer customer) {
-		customer.setId(0);
+	@PostMapping("/save")
+	public String saveCustomer(@ModelAttribute("customer") Customer customer) {
 		customerService.save(customer);
-		return customer;
+		return "redirect:/customers/list";
 	}
 
-	@PutMapping("/customers")
-	public Customer updateCustomer(@RequestBody Customer customer) {
-		customerService.save(customer);
-		return customer;
+	@GetMapping("/addForm")
+	public String addForm(Model model) {
+		Customer customer = new Customer();
+		model.addAttribute("customer", customer);
+		return "customers/customer-form";
 	}
+
+	@GetMapping("/updateForm")
+	public String updateForm(@RequestParam("customerId") int id, Model model) {
+		Customer customer = customerService.getCustomer(id);
+		model.addAttribute("customer", customer);
+		return "customers/customer-form";
+	}
+
 	
-	@DeleteMapping("/customers/{customerId}")
-	public String deleteCustomer(@PathVariable int customerId) {
-		Customer customer = customerService.getCustomer(customerId);
-		if(customer == null) {
-			throw new RuntimeException("Customer is not found with id :" + customerId);
-		}
-		customerService.deleteById(customerId);
-		return "Customer is deleted with id " + customerId;
+	@GetMapping("/delete")
+	public String deleteCustomer(@RequestParam("customerId") int id) {
+	
+		customerService.deleteById(id);
+		return "redirect:/customers/list";
 	}
 
 }
