a3be4aabbd74fcdaf375ce1e4481d963c003efc2
liq19ch <s4031630087@gmail.com>
2022-03-24 01:56:53 +0800

    version2: use standard JPA

--- src/main/java/com/crm/demo/dao/CustomerDAOImpl.java
+++ src/main/java/com/crm/demo/dao/CustomerDAOImpl.java
@@ -3,9 +3,10 @@ package com.crm.demo.dao;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 import org.hibernate.Session;
-import org.hibernate.query.Query;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,36 +24,29 @@ public class CustomerDAOImpl implements CustomerDAO {
 	}
 
 	@Override
-	@Transactional
 	public List<Customer> getCustomers() {
 		
-		Session currentSession = entityManager.unwrap(Session.class);
-		Query<Customer> query = currentSession.createQuery("from Customer", Customer.class);
+		Query query = entityManager.createQuery("from Customer");
 		List<Customer> customers = query.getResultList();
 		
 		return customers;
 	}
 
 	@Override
-	@Transactional
 	public Customer getCustomer(int id) {
-		Session currentSession = entityManager.unwrap(Session.class);
-		Customer customer = currentSession.get(Customer.class, id);
+		Customer customer = entityManager.find(Customer.class, id);
 		return customer;
 	}
 
 	@Override
-	@Transactional
 	public void save(Customer customer) {
-		Session currentSession = entityManager.unwrap(Session.class);
-		currentSession.saveOrUpdate(customer);
+		Customer dbCustomer = entityManager.merge(customer);
+		customer.setId(dbCustomer.getId());
 	}
 
 	@Override
-	@Transactional
 	public void deleteById(int id) {
-		Session currentSession = entityManager.unwrap(Session.class);
-		Query query = currentSession.createQuery("delete from Customer where id=:customerId");
+		Query query = entityManager.createQuery("delete from Customer where id=:customerId");
 		query.setParameter("customerId", id);
 		query.executeUpdate();
 	}
