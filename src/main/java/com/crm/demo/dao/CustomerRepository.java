30b18f3f5e357e862a7cdd933e205ff995d1dcde
liq19ch <s4031630087@gmail.com>
2022-03-24 04:19:47 +0800

    jpa repository add order by last name func

--- src/main/java/com/crm/demo/dao/CustomerRepository.java
+++ src/main/java/com/crm/demo/dao/CustomerRepository.java
@@ -1,9 +1,14 @@
 package com.crm.demo.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.crm.demo.entity.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-
+	
+	
+	
+	public List<Customer> findAllByOrderByLastNameAsc();	
 }
